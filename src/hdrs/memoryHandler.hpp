33fd4d70b614ba62faa84e0fded1c44f4d993b43
shammy <jose.shamm@gmail.com>
2018-09-23 18:23:26 -0600

    added id list functionality

--- src/hdrs/memoryHandler.hpp
+++ src/hdrs/memoryHandler.hpp
@@ -6,11 +6,14 @@ class MemoryHandler {
 public:
 	MemoryHandler();
 	std::string allocate4BytePrimitive(int);
-	std::string allocateString(const std::string&, const std::string type= "str");
+	std::string allocateString(const std::string&, const std::string type= "$str");
 	std::string generateDataCode();
+	std::string allocateInt(const int&, const std::string type= "$numb");
 private:
-	int constCount, tempCount, stringCount;
-	std::unordered_map<std::string, int>inMemoryInts;
+	std::string getStringPreambles();
+	std::string getIntPreambles();
+	int constCount, tempCount, stringCount, numberCount;
+	std::unordered_map<int, std::string>inMemoryInts;
 	std::unordered_map<std::string, std::string>inMemoryStrings;
 };
 #endif
--- src/memoryHandler.cpp
+++ src/memoryHandler.cpp
@@ -1,6 +1,6 @@
 #include "memoryHandler.hpp"
 
-MemoryHandler::MemoryHandler() : constCount(0), tempCount(0), stringCount(0){}
+MemoryHandler::MemoryHandler() : constCount(0), tempCount(0), stringCount(0), numberCount(0){}
 
 std::string MemoryHandler::allocateString(const std::string& newString, const std::string type){
 	const auto& str_map_it= inMemoryStrings.find(newString);
@@ -9,7 +9,7 @@ std::string MemoryHandler::allocateString(const std::string& newString, const st
 	}
 	std::ostringstream place;
 	place<<type
-		<< ((type=="temp") ? ++tempCount:++stringCount);
+		<< ((type=="$temp") ? ++tempCount:++stringCount);
 	std::string placeString= place.str();
 	inMemoryStrings.emplace(newString, placeString);
 	return placeString;
@@ -17,10 +17,37 @@ std::string MemoryHandler::allocateString(const std::string& newString, const st
 
 std::string MemoryHandler::generateDataCode(){
 	std::ostringstream result;
-	result<<"section .data"<<std::endl;
+	result<<"section .data"<<std::endl
+		<<getStringPreambles()
+		<<getIntPreambles();
+	return result.str();
+}
+
+std::string MemoryHandler::getStringPreambles(){
+	std::ostringstream result;
 	for (auto &str : inMemoryStrings){
 		result<<str.second<<" db \""<<str.first<<"\", 0"<<std::endl;
 	}
-	
 	return result.str();
+}
+
+std::string MemoryHandler::getIntPreambles(){
+	std::ostringstream result;
+	for (auto &integ : inMemoryInts){
+		result<<integ.second<<" dd "<<integ.first<<std::endl;
+	}
+	return result.str();
+}
+
+std::string MemoryHandler::allocateInt(const int& newInt, const std::string type){
+	const auto& str_map_it= inMemoryInts.find(newInt);
+	if ( str_map_it != inMemoryInts.end()){
+		return str_map_it->second;
+	}
+	std::ostringstream place;
+	place<<type
+		<< ((type=="$temp") ? ++tempCount:++numberCount);
+	std::string placeString= place.str();
+	inMemoryInts.emplace(newInt, placeString);
+	return placeString;
 }
